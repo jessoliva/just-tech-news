d52acc87149a5436c4ebab8610ed38294399584c
jessoliva <95152563+jessoliva@users.noreply.github.com>
2022-03-17 15:40:09 -0500

    created Post table and relationship between User table and Post table through user_id as foreign key in Post table

---
+++ models/Post.js
@@ -0,0 +1,47 @@
+const { Model, DataTypes } = require('sequelize');
+// connection to MySQL we stored in the connection.js
+const sequelize = require('../config/connection');
+
+// create(define) our Post model
+class Post extends Model {}
+
+// create fields/columns for Post model
+Post.init(
+    { // in this first parameter for the Post.init function, we define the Post schema
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        post_url: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: { // we ensure that this url is a verified link by setting the isURL property to true
+                isURL: true
+            }
+        },
+        user_id: { // used to create a relationship between user table and post table
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+        // this column determines who posted the news article
+        // Using the references property, we establish the relationship between this post and the user by creating a reference to the User model, specifically to the id column that is defined by the key property, which is the primary key
+        // The user_id is conversely defined as the foreign key in the Post table and will be the matching link
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post'
+    }
+);
+
+module.exports = Post;
--- models/index.js
+++ models/index.js
@@ -1,4 +1,10 @@
-// collect and export the User model data
+// collect and export the models data
 // export it as an object with it as a property
+
 const User = require('./User');
-module.exports = { User };
+const Post = require('./Post');
+
+module.exports = { User, Post };
+
+// User --> primary key id
+// Post --> foreign key (user_id) references user(id)
