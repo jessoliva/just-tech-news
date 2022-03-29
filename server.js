e9abacf628b638ce6eae4beab82a1d3a336514b0
jessoliva <95152563+jessoliva@users.noreply.github.com>
2022-03-28 23:18:20 -0500

    used dummy data to render homepage

--- server.js
+++ server.js
@@ -1,20 +1,36 @@
-// installed dependencies: npm install express sequelize mysql2
+// installed dependencies: npm install express sequelize mysql2 express-handlebars
 
+// express
 const express = require('express');
-// references routes/index.js file!
-const routes = require('./routes');
+const app = express();
+const routes = require('./controllers'); // references routes/index.js file!
+const path = require('path');
+const PORT = process.env.PORT || 3001;
+
+// sequelize
 // importing the connection to Sequelize from config/connection.js. Then, at the bottom of the file, we use the sequelize.sync() method to establish the connection to the database
 const sequelize = require('./config/connection');
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+// express-handlebars
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
+// express-handlebars engine setup
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// use static files in public folder
+app.use(express.static(path.join(__dirname, 'public')));
+// The express.static() method is a built-in Express.js middleware function that can take all of the contents of a folder and serve them as static assets. This is useful for front-end specific files like images, style sheets, and JavaScript files
 
 // turn on routes
 app.use(routes);
 // Since we set up the routes the way we did, we don't have to worry about importing multiple files for different endpoints. The router instance in routes/index.js collected everything for us and packaged them up for server.js to use
+
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => { // switch back and forth btw true and false - true when you need to drop db, false when that has happened already
